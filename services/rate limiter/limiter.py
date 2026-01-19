f19f26794fd175e0ef880f2e4dcd8265e197bdd8
Neeraj Chandel <nchan@Neerajs-MacBook-Pro.local>
2026-01-19 11:56:34 -0800

    updating limiter test calls

--- services/rate limiter/limiter.py
+++ services/rate limiter/limiter.py
@@ -2,9 +2,14 @@ from flask import Flask, jsonify, request
 import time
 import requests
 
-HOME_URL = "http://127.0.0.1"
 COUNTER_PORT = "5001"
+CACHE_PORT = "6001"
+
+HOME_URL = "http://127.0.0.1"
 COUNTER_URL = HOME_URL + ":" + COUNTER_PORT
+CACHE_URL = HOME_URL + ":" + CACHE_PORT
+
+TOKEN_LIMIT = 10
 
 app = Flask(__name__)
 
@@ -18,19 +23,30 @@ class Limiter:
         response.raise_for_status()
         return response.json()["epoch_start"]
     
+    def create_tokens(self, user_id):
+        # we build our payload and create a last_refill_ts
+        # which is last refilled timestamp for the user that tells
+        # when was its token bucket last refilled
+        payload = {
+            "tokens": TOKEN_LIMIT,
+            "last_refill_ts": time.time()
+        }
+        # call the set-cache API and send our payload so that we can
+        # store it in the cache
+        response = requests.post(CACHE_URL + "/set-cache/" + user_id, json = payload)
+        return jsonify(response)
+    
     def fetch_tokens(self, user_id):
-        # our cache is the source of truth
-        # over here - but when do we actually
-        # update the cache?
-        # and what about start-up situations?
-        
-        return
+        # see if we get anything for the user request
+        response = requests.get(CACHE_URL + "/get-cache/" + user_id)
+        print(response)
+        # if we get a cache miss, we simply create new tokens for the user:
+        if response.status_code == 404:
+            print("Cache Miss; creating tokens for the user_id" + user_id)
+            self.create_tokens(user_id=user_id)
+        return response
     
     def update_tokens(self, user_id):
-        # how do we manage the updation of tokens?
-        # an async call needs to be made every 5 minutes
-        # (according to the rate calculations)
-        # and will that be a process that runs in the background?
         return
     
     def build_record(self, request, counter_url):
@@ -72,13 +88,24 @@ def test_epoch():
 
 @app.route("/stamped-request", methods=["POST"])
 def stamp_request():
+    stamped_request = None
     try:
         stamped_request = limiter.build_record(request, COUNTER_URL)
+
+        """
+        ------------ Beginning of test code ------------
+        """
+        user_id = stamped_request["user_id"]
+        response = limiter.fetch_tokens(user_id)
+        """
+        ------------ End of test code ------------
+        """
+            
         return jsonify(stamped_request)
     except ValueError as e:
         return jsonify({"error": str(e)}), 400
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
+    
 if __name__ == "__main__":
     app.run(port=5002, debug=True)
